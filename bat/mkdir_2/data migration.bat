cde6c8d7a3777be715c8589fb14d9e800d870ff6
Alexious Yong <alexiousyong@outlook.com>
2021-09-28 19:31:54 +0800

    Create data migration.bat

---
+++ bat/mkdir_2/data migration.bat
@@ -0,0 +1,155 @@
+Rem Massaged Part
+md Massaged\@Scripts\Phase_1\Ind_Mem
+md Massaged\@Scripts\Phase_1\Corp_Ind
+md Massaged\@Scripts\Phase_1\Corp_Group
+
+md Massaged\@Scripts\Phase_2
+
+md Massaged\@Scripts\Phase_2a
+md Massaged\@Scripts\Phase_2a\AMI
+md Massaged\@Scripts\Phase_2a\GEL
+md Massaged\@Scripts\Phase_2a\GEM
+md Massaged\@Scripts\Phase_2a\GET
+md Massaged\@Scripts\Phase_2a\GROUP
+md Massaged\@Scripts\Phase_2a\IHM
+md Massaged\@Scripts\Phase_2a\KUR
+md Massaged\@Scripts\Phase_2a\PIB
+md Massaged\@Scripts\Phase_2a\TPA
+md Massaged\@Scripts\Phase_2a\UNI
+
+md Massaged\@Scripts\Phase_2b\Ind_Mem
+md Massaged\@Scripts\Phase_2b\Corp_Ind
+md Massaged\@Scripts\Phase_2b\Corp_Group
+
+md Massaged\@Scripts\Phase_3
+
+md Massaged\@Scripts\Phase_4
+md Massaged\@Scripts\Phase_4\Case
+md Massaged\@Scripts\Phase_4\Claim
+md Massaged\@Scripts\Phase_4\Guarantee_Letter
+
+md Massaged\Phase_1\Ind_Mem
+md Massaged\Phase_1\Corp_Ind
+md Massaged\Phase_1\Corp_Group
+
+md Massaged\Phase_2a
+md Massaged\Phase_2a\AMI
+md Massaged\Phase_2a\GEL
+md Massaged\Phase_2a\GEM
+md Massaged\Phase_2a\GET
+md Massaged\Phase_2a\GROUP
+md Massaged\Phase_2a\IHM
+md Massaged\Phase_2a\KUR
+md Massaged\Phase_2a\PIB
+md Massaged\Phase_2a\TPA
+md Massaged\Phase_2a\UNI
+
+md Massaged\Phase_2b\Ind_Mem
+md Massaged\Phase_2b\Corp_Ind
+md Massaged\Phase_2b\Corp_Group
+
+md Massaged\Phase_3
+
+md Massaged\Phase_4
+md Massaged\Phase_4\Case
+md Massaged\Phase_4\Claim
+md Massaged\Phase_4\Guarantee_Letter
+md Massaged\Phase_4\
+
+Rem Match_Excel
+md MATCH_EXCEL\Phase_1\Ind_Mem
+md MATCH_EXCEL\Phase_1\Corp_Ind
+md MATCH_EXCEL\Phase_1\Corp_Group
+
+md MATCH_EXCEL\Phase_2
+
+md MATCH_EXCEL\Phase_2a
+md MATCH_EXCEL\Phase_2a\AMI
+md MATCH_EXCEL\Phase_2a\GEL
+md MATCH_EXCEL\Phase_2a\GEM
+md MATCH_EXCEL\Phase_2a\GET
+md MATCH_EXCEL\Phase_2a\GROUP
+md MATCH_EXCEL\Phase_2a\IHM
+md MATCH_EXCEL\Phase_2a\KUR
+md MATCH_EXCEL\Phase_2a\PIB
+md MATCH_EXCEL\Phase_2a\TPA
+md MATCH_EXCEL\Phase_2a\UNI
+
+md MATCH_EXCEL\Phase_2b\Ind_Mem
+md MATCH_EXCEL\Phase_2b\Corp_Ind
+md MATCH_EXCEL\Phase_2b\Corp_Group
+
+md MATCH_EXCEL\Phase_3
+
+md MATCH_EXCEL\Phase_4
+md MATCH_EXCEL\Phase_4\Case
+md MATCH_EXCEL\Phase_4\Claim
+md MATCH_EXCEL\Phase_4\Guarantee_Letter
+
+Rem Raw
+md Raw\@Scripts\Phase_1\Ind_Mem
+md Raw\@Scripts\Phase_1\Corp_Ind
+md Raw\@Scripts\Phase_1\Corp_Group
+
+md Raw\@Scripts\Phase_2
+
+md Raw\@Scripts\Phase_2a
+md Raw\@Scripts\Phase_2a\AMI
+md Raw\@Scripts\Phase_2a\GEL
+md Raw\@Scripts\Phase_2a\GEM
+md Raw\@Scripts\Phase_2a\GET
+md Raw\@Scripts\Phase_2a\GROUP
+md Raw\@Scripts\Phase_2a\IHM
+md Raw\@Scripts\Phase_2a\KUR
+md Raw\@Scripts\Phase_2a\PIB
+md Raw\@Scripts\Phase_2a\TPA
+md Raw\@Scripts\Phase_2a\UNI
+
+md Raw\@Scripts\Phase_2b\Ind_Mem
+md Raw\@Scripts\Phase_2b\Corp_Ind
+md Raw\@Scripts\Phase_2b\Corp_Group
+
+md Raw\@Scripts\Phase_3
+
+md Raw\@Scripts\Phase_4
+md Raw\@Scripts\Phase_4\Case
+md Raw\@Scripts\Phase_4\Claim
+md Raw\@Scripts\Phase_4\Guarantee_Letter
+
+md Raw\Phase_1\Ind_Mem
+md Raw\Phase_1\Corp_Ind
+md Raw\Phase_1\Corp_Group
+
+md Raw\Phase_2a
+md Raw\Phase_2a\AMI
+md Raw\Phase_2a\GEL
+md Raw\Phase_2a\GEM
+md Raw\Phase_2a\GET
+md Raw\Phase_2a\GROUP
+md Raw\Phase_2a\IHM
+md Raw\Phase_2a\KUR
+md Raw\Phase_2a\PIB
+md Raw\Phase_2a\TPA
+md Raw\Phase_2a\UNI
+
+md Raw\Phase_2b\Ind_Mem
+md Raw\Phase_2b\Corp_Ind
+md Raw\Phase_2b\Corp_Group
+
+md Raw\Phase_3
+
+md Raw\Phase_4
+md Raw\Phase_4\Case
+md Raw\Phase_4\Claim
+md Raw\Phase_4\Guarantee_Letter
+
+
+
+
+
+
+
+
+
+
+
